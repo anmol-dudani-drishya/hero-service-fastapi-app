2a8edce4dbc1e0e32d62616cc546c5310114f244
anmol-dudani-drishya <anmol.dudani@drishya.ai>
2022-01-18 13:51:36 +0530

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,10 +1,8 @@
 import uvicorn
-from fastapi import FastAPI, HTTPException
 from src.app import app
 from mangum import Mangum
 
-apper = FastAPI()
-handler = Mangum(apper)
+handler = Mangum(app)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
