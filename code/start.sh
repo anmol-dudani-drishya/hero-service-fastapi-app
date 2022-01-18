06af0327a1b679906faff2c059167e762cbe88d6
anmol-dudani-drishya <anmol.dudani@drishya.ai>
2022-01-18 15:39:55 +0530

    testing edits

--- code/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python3 -m venv .venv
-source .venv/bin/activate
-python3 -m pip install --upgrade pip
-python3 -m pip install pipenv
-python3 -m pipenv install --dev --pre
-python3 vscode-env-setup.py
-uvicorn main:app --reload
